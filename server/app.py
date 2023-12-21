96be98c842a018ced8e985fa5d19ac82991f4298
oneminute <oneminute3b@gmail.com>
2023-12-21 22:30:05 +0000

    add robots.txt

--- server/app.py
+++ server/app.py
@@ -1,4 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
+import psycopg2
+
+db_params = {
+    'database': 'aiworkflow',
+    'user': 'postgres',
+    'password': '690854Lr',
+    'host': 'localhost',  # or your server IP if remote
+    'port': 5432  # default PostgreSQL port
+}
 
 app = Flask(__name__,
             static_url_path='', 
@@ -8,6 +17,40 @@ app = Flask(__name__,
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route("/db_helper/tasks/get")
+def get_tasks():
+    # Connect to the PostgreSQL database
+    try:
+        conn = psycopg2.connect(**db_params)
+        cursor = conn.cursor()
+
+        # SQL query to fetch data from the t_tasks table
+        query = 'SELECT * FROM t_tasks'
+
+        # Execute the query
+        cursor.execute(query)
+
+        # Fetch all the records
+        records = cursor.fetchall()
+        result = []
+        for row in records:
+            result.append(row)
+
+        # Close the cursor and connection
+        cursor.close()
+        conn.close()
+
+        return jsonify(result)
+
+    except psycopg2.Error as e:
+        print("Error: " + e)
+    finally:
+        if conn is not None:
+            conn.close()
+    
     
 if __name__ == "__main__":
     app.run(ssl_context='adhoc', port=5000)
+
+# print(get_tasks())
